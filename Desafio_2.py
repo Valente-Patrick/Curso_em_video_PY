935d151b8d1412dbc810b989a37d12a27daccc4f
Patrick Valente <valente.patrick.dev@gmail.com>
2024-03-23 17:53:43 -0300

    desafio 2

---
+++ Desafio_2.py
@@ -0,0 +1,5 @@
+dia = input("Qual o dia do seu aniversário?")
+mes = input("Qual o mês do seu aniversário?")
+ano = input("Em que ano você nasceu?")
+
+print("Você nasceu no dia" , dia , "de" , mes , "de" , ano , ". Correto?")
