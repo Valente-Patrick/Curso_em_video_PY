ec5b60b653980d7e07e45ee07dcfa1480f7a795e
Patrick Valente <valente.patrick.dev@gmail.com>
2024-03-24 04:45:00 -0300

    update

--- Desafios_de_codigo/Desafio_4-1.py
+++ Desafios_de_codigo/Desafio_4-1.py
@@ -4,5 +4,3 @@ print('{} é alfabético? '.format(a), a.isalpha())
 print('{} é alphanumérico? '.format(a), a.isalnum())
 print('{} é só números? '.format(a), a.isnumeric())
 print('{} está tudo em maíusculo? '.format(a), a.isupper())
-
-#para uasr o .format, ele sempre tem que vir antes da verificação
